045fb08f9d38a4184039d4ff9a62ad6e65c1144c
Sonarpratik <sonarpratik02.ps@gmail.com>
2023-04-23 12:07:20 +0530

    new about npm

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const mongoPath ='mongodb+srv://profile:1234@cluster0.7nuth3a.mongodb.net/some?retryWrites=true&w=majority'
+//mongodb+srv://profile:<password>@cluster0.7nuth3a.mongodb.net/?retryWrites=true&w=majority
 
 module.exports= async ()=>{
     await mongoose.connect(mongoPath ,{
